d92d07af5905e7dab9210346f29cc0a096c654c1
Intortion <144892095+Intortion@users.noreply.github.com>
2023-09-19 15:43:08 +0530

    Update q3.py
    Same code but using classes

--- q3.py
+++ q3.py
@@ -1,29 +1,45 @@
-# input
-l = []
-print("Enter a number to add to list or press q to quit: ")
-while True:
-    n = input()
-    if n == 'q':
-        break
-    else:
-        l.append(int(n))
 
-def powerset(l):
-    new_l = []
+class Power:
 
-    # if last element
-    if len(l) < 1:
-        return [[]]
-
-    for rem_powerset_l in powerset(l[1:]):  # recursing in rest list
-        new_l.append(rem_powerset_l + [l[0]])
-        new_l.append(rem_powerset_l)
+    def __init__(self):
+        self.l = []
+        self.new_l = []
     
-    return new_l
+    def input(self):
 
-p_l = powerset(l)
-# sorting the output
-for j in p_l:
-    j = j.sort()
-p_l.sort()
-print(p_l)
+        print("Enter a number to add to list or press q to quit: ")
+        while True:
+            n = input()
+            if n == 'q':
+                break
+            else:
+                self.l.append(int(n)) 
+        
+    def powerset(self,l):
+
+        # if last element
+        if len(l) < 1:
+            return [[]]
+
+        power_l = []
+        for rem_powerset_l in self.powerset(l[1:]):  # recursing in rest list
+            power_l.append(rem_powerset_l + [l[0]])
+            power_l.append(rem_powerset_l)
+        
+        return power_l
+
+    def output(self):
+        # sorting the output
+        for j in self.new_l:
+            j = j.sort()
+        self.new_l.sort()
+
+        return self.new_l
+    
+power = Power()
+
+power.input()
+
+power.new_l = power.powerset(power.l)
+
+print(power.output())
